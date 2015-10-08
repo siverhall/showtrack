9b206960e2b7b200407369ee825de2318681b499
siverhall <emil.siverhall@gmail.com>
2015-10-08 12:11:11 +0200

    added maven dependency for guice-repository

--- src/main/java/com/siverhall/ShowTrackApp.java
+++ src/main/java/com/siverhall/ShowTrackApp.java
@@ -5,6 +5,10 @@ import com.siverhall.pages.StartPage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
+/**
+ *  The starting point of the application. Decides which page to go to as homepage
+ *  and handles Wicket-specific setup
+ */
 public class ShowTrackApp extends WebApplication
 {
 
